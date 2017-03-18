303d40ae7c2aee1f42fe69d07296fd933f1e93db
Gize <Gize@172.17.5.60>
2017-03-17 23:45:25 -0500

    test

--- Graph/src/lab12/ConnectedComponentSearch.java
+++ Graph/src/lab12/ConnectedComponentSearch.java
@@ -1,8 +1,21 @@
 package lab12;
 
 //implement
+import java.util.List;
+
 public class ConnectedComponentSearch extends DepthFirstSearch {
-	public ConnectedComponentSearch(Graph graph) {
-		super(graph);
+
+    List<Graph> conGrap;
+    private int componentNum = 0;
+
+    public ConnectedComponentSearch(Graph graph) {
+        super(graph);
+    }
+    List<Graph> computeConnnectedComponents(){
+		//implement here
+		return conGrap;
+	}
+	public int getComponentNum(){
+		return componentNum;
 	}
 }
